9f412f510469bf74be1fcf1f7e3ffcbdcc42906f
Ryan Scott <r.l.scott2014@gmail.com>
2025-03-01 20:10:20 -0500

    configure for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
+const isProd = process.env.NODE_ENV === 'production';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+  reactStrictMode: true,
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  // basePath: isProd ? "/ryanscott96.github.io" : "",
+  // assetPrefix: isProd ? "/ryanscott96.github.io" : "",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
